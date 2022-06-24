7f416d9e5bc4562f41ceb5e25e215c974ecedadc
Danillo Rodrigues <danilo100kl@hotmail.com>
2022-06-24 20:14:47 -0300

    feat: add payment method

--- v1/modules/payments/payments.ts
+++ v1/modules/payments/payments.ts
@@ -1,9 +1,22 @@
 import { Request, Response } from "express";
+import dotenv from 'dotenv';
 import validator from 'validator';
+import { ConstructorInterface, PaymentRequestInterface, Picpay } from 'picpay-sdk';
+
+dotenv.config();
+
+const constructorInterface: ConstructorInterface = {
+  picpayToken: String(process.env.PICPAY_TOKEN),
+  sellerToken: String(process.env.SELLER_TOKEN)
+} 
+
+console.log(constructorInterface)
+console.log(process.env.PORT)
+
+const picpay = new Picpay(constructorInterface)
 
 export class Payment {
-
-   public static buyCrypto(req: Request, res: Response): void {
+   public static async buyCrypto(req: Request, res: Response): Promise<void> {
 
     const {
       fiatAmount, 
@@ -26,6 +39,21 @@ export class Payment {
     if(!validator.isNumeric(fiatAmount)){
       res.status(400).send('Fiat amount is not valid');
     }
+
+    const requestParams: PaymentRequestInterface = {
+      buyer: {
+          document,
+          email,
+          firstName,
+          lastName,
+          phone,
+      },
+      value: fiatAmount,
+      referenceId: (new Date()).getTime().toString() ,
+      callbackUrl: 'https://callback',
+    };
+
+    await picpay.payment.request(requestParams).then((data)=>{ console.log(data) }).catch((err)=>{ console.log(err) });
 
     res.status(201).send('Send with successful ');
   }
